8943ca6644bd81801c141745bc49c3812681d829
abuki <abukiw86@gmail.com>
2026-04-02 11:59:55 +0100

    sql modified

--- freenet.sql
+++ freenet.sql
@@ -1,70 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: sql100.byetcluster.com
--- Generation Time: Apr 02, 2026 at 04:51 AM
--- Server version: 11.4.10-MariaDB
--- PHP Version: 7.2.22
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ezyro_41560557_freenet`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL,
-  `role` enum('user','admin') NOT NULL DEFAULT 'user',
-  `pass` varchar(255) NOT NULL,
-  `date` timestamp NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `mail`, `phone`, `role`, `pass`, `date`) VALUES
-(1, 'adminonlynotuser@freenet.com', '0981566599', 'admin', 'are you ready for users ?', '2026-04-02 07:00:00');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `freenet`.`users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `mail` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(255) NOT NULL,
+    `role` ENUM('user','admin') NOT NULL DEFAULT 'user',
+    `pass` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
